38234bcb2372710a6e12999003a2d61de3ce872f
Raakost <jeanettesydboege@gmail.com>
2018-04-15 17:21:20 +0200

    renaming

--- src/models/TabsHomePageModel.ts
+++ src/models/TabsHomePageModel.ts
@@ -1,18 +1,18 @@
 export class TabsHomePageModel {
 
-  constructor(id: number, text: string, slideImg1: string, slideImg2: string, longitude: number, latitude: number) {
+  constructor(id: number, text: string, beforeImg: string, nowImg: string, longitude: number, latitude: number) {
     this.Id = id;
     this.Text = text;
-    this.SlideImg1 = slideImg1;
-    this.SlideImg2 = slideImg2;
+    this.BeforeImg = beforeImg;
+    this.NowImg = nowImg;
     this.Longitude = longitude;
     this.Latitude = latitude;
   }
 
   Id: number;
   Text: string;
-  SlideImg1: string;
-  SlideImg2: string;
+  BeforeImg: string;
+  NowImg: string;
   Longitude: number;
   Latitude: number;
 
