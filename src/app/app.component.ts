d870a7942e084a9caeb7836a688324a88cab7ea8
Raakost <jeanettesydboege@gmail.com>
2018-07-01 21:49:21 +0200

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -68,5 +68,3 @@ export class MyApp {
     this.nav.push(page.component, page.data);
   }
 }
-
-/*<preference name="StatusBarBackgroundColor" value="#193441"/> <preference name="StatusBarStyle" value="#FFFFFF" />*/
